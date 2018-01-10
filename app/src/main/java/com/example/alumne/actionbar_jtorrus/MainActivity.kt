27e09a49eb5c2c8bcb2eb2384981e546162616e3
JTorrus <jtorrusgalindo@gmail.com>
2018-01-10 19:17:31 +0100

    Method to fill a String array from res implemented

--- app/src/main/java/com/example/alumne/actionbar_jtorrus/MainActivity.kt
+++ app/src/main/java/com/example/alumne/actionbar_jtorrus/MainActivity.kt
@@ -4,9 +4,25 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var data: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fillDataToArr()
+    }
+
+    fun fillDataToArr() {
+        data = ArrayList<String>()
+
+        val stringsToUse = this.resources.getStringArray(R.array.strings_to_use)
+        var item: String
+
+        for (i in 0 until stringsToUse.size) {
+            item = stringsToUse[i]
+            data.add(item)
+        }
+
     }
 }
