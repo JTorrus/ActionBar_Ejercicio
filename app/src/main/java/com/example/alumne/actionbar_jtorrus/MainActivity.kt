924ec9daa523cbd314099a081757e6e6913e97f5
JTorrus <jtorrusgalindo@gmail.com>
2018-01-10 20:59:30 +0100

    Action bars icon added, no functionality

--- app/src/main/java/com/example/alumne/actionbar_jtorrus/MainActivity.kt
+++ app/src/main/java/com/example/alumne/actionbar_jtorrus/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.alumne.actionbar_jtorrus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -32,5 +33,14 @@ class MainActivity : AppCompatActivity() {
     fun showListView() {
         val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, fillDataToArr())
         list_view.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (super.onCreateOptionsMenu(menu)) {
+            menuInflater.inflate(R.menu.menu, menu)
+            return true
+        } else {
+            return false
+        }
     }
 }
